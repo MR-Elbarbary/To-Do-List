71aac31dd9a09693be7f66f1910060f34610d2bc
MR-Elbarbary <elbarbary7m@gmail.com>
2024-09-02 00:06:53 +0300

    Add Css and JS for Dialog

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./style.css";
+
+document.addEventListener("DOMContentLoaded",()=>{
+    let newTaskBtn = document.getElementById("newTask");
+    let newTaskForm = document.querySelector("dialog");
+    newTaskBtn.addEventListener("click", ()=>{
+        newTaskForm.showModal();
+    });
+});
